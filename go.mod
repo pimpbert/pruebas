aba349ec439038fb6b8f589e064480a9e017ab4e
Gilberto Paz Cruz <gipaz@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-06-28 14:15:00 -0500

    se agrega goodbye.go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module pruebas
+
+go 1.16
+
+require github.com/pimpbert/pruebas v0.0.0-20210625193920-d95e863de4c1
--- main.go
+++ main.go
@@ -4,12 +4,20 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/pimpbert/pruebas/handlers"
 )
 
 func main() {
 
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
 
-	http.ListenAndServe(":9090", nil)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	http.ListenAndServe(":9090", sm)
 }
